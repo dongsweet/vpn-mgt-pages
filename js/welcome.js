b37e78f8d4536dbeff599ef95f35b76c37b23f35
Qing Dong <sweetreg@126.com>
2021-06-17 00:17:36 +0800

    配合后端修改

--- js/welcome.js
+++ js/welcome.js
@@ -5,8 +5,7 @@
 
 /* eslint-disable camelcase */
 
-const INNER_RES_LIST_BY_BRANCH = "/api/innerResource/listByBranch";
-const VPN_USER_INFO = "/api/vpnUser/getUserByIp";
+const INNER_RES_LIST = "/api/innerResource/getUserResources";
 
 (function ($) {
     'use strict'
@@ -21,15 +20,15 @@ const VPN_USER_INFO = "/api/vpnUser/getUserByIp";
     }
 
     function fillMainContent(data) {
-        data.forEach(d => {
+        data.classified.forEach(d => {
             let row = $('<div class="row"></div><!-- /.row -->');
-            $('#welcome-container').append(`<h5 class="mb-2">${d.branch}</h5>`, row);
+            $('#welcome-container').append(`<h5 class="mb-2">${d.clazz}</h5>`, row);
             
             d.resources.forEach(r => {
                 let col = $('<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3"></div><!-- /.col -->');
                 let infobox = $('<div class="info-box"></div><!-- /.info-box -->');
                 let content = $('<div class="info-box-content"></div><!-- /.info-box-content -->');
-                content.append(`<span class="info-box-text">${r.type + (r.tag ? '(' + r.tag + ')' : '')}</span>`);
+                content.append(`<span class="info-box-text">${r.name + (r.tag ? '(' + r.tag + ')' : '')}</span>`);
                 content.append(`<span class="info-box-text">服务地址：${r.service_domain ? r.service_domain : r.service_ip}</span>`);
                 content.append(`<span class="info-box-text">真实地址：${r.real_ip ? r.real_ip : r.service_ip}</span>`);
                 content.append(`<span class="info-box-text">${r.service_url ? '<a href="' + r.service_url + '" target="_blank">单击访问</a>': '使用工具访问'}</span>`);
@@ -44,28 +43,19 @@ const VPN_USER_INFO = "/api/vpnUser/getUserByIp";
                 row.append(col);
             });
         });
+
+        $('#vpnUserRealName').text(data.realname);
+        $('#vpnUserName').text(data.username);
+        $('#vpnIp').text(data.ip);
+        $('#vpnUserGroup').text(data.group_path);
     }
 
-    //加载用户信息
-    $.getJSON(VPN_USER_INFO)
-    .done(data => {
-        $("#vpnUserRealName").text(data.realname);
-        $("#vpnUserName").text(data.username);
-        $("#vpnIp").text(data.ip);
-    }).fail((jqXHR, textStatus, err) => {
-        console.error(err);
-    });
-
     //加载导航数据
-    $.getJSON(INNER_RES_LIST_BY_BRANCH)
+    $.getJSON(INNER_RES_LIST)
     .done(data => {
         fillMainContent(data);
     }).fail((jqXHR, textStatus, err) => {
         console.error(err);
     });
-
-
-
-    
 
 })(jQuery)
