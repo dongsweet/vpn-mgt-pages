96424ccb8c6c5cc4f4589d7fbae3b32e07d3d3e2
Qing Dong <sweetreg@126.com>
2021-07-08 09:28:53 +0800

    新增资源管理页面

---
+++ admin/js/login.js
@@ -0,0 +1,113 @@
+
+const CAPTCHA = '/api/captcha';
+const PUBLIC_KEY = '/api/adminUser/publicKey';
+const LOGIN = '/api/adminUser/login';
+
+(function ($) {
+    'use strict'
+
+    const encrypt = new JSEncrypt();
+    var csrftoken = Cookies.get('csrfToken');
+
+    function csrfSafeMethod(method) {
+        // these HTTP methods do not require CSRF protection
+        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    }
+    $.ajaxSetup({
+        beforeSend: function (xhr, settings) {
+            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader('x-csrf-token', csrftoken);
+            }
+        },
+    });
+
+    function loadCaptcha() {
+        let captchaUrl = CAPTCHA + '?' + Math.random();
+        $("#imgCaptcha").attr('src', captchaUrl);
+    }
+
+    function showErrMsg(msg) {
+        $("#spanError").text(msg);
+        $("#divError").show();
+    }
+
+    $('#formLogin').validate({
+        submitHandler: function () {
+            let dataArray = $('#formLogin').serializeArray();
+            let data = {};
+            dataArray.forEach(element => {
+                data[element.name] = element.value;
+            });
+
+            $.get(PUBLIC_KEY).done(pubKey => {
+                encrypt.setPublicKey(pubKey);
+                console.log(`Post data: ${JSON.stringify(data)}`);
+                let encData = {
+                    enc: encrypt.encrypt(JSON.stringify(data))
+                };
+
+                $.post(LOGIN, encData).done(loginRes => {
+                    
+                    if(!loginRes.result) {
+                        showErrMsg(loginRes.msg);
+                        loadCaptcha();
+                    } else {
+                        // 跳转
+                        $("#divError").hide();
+                        $(window).attr('location', 'resources.html');
+                    }
+                }).fail((jqXHR, textStatus, err) => {
+                    // TODO 在页面上添加错误消息
+                    console.log(err);
+                    showErrMsg('登录信息提交失败');
+                });
+            }).fail((jqXHR, textStatus, err) => {
+                // TODO 在页面上添加错误消息
+                console.log(err);
+                showErrMsg('秘钥信息获取失败');
+            });
+        },
+        rules: {
+            username: {
+                required: true,
+                email: true,
+            },
+            password: {
+                required: true,
+                minlength: 5
+            },
+            captcha: {
+                required: true,
+                rangelength: [4, 4]
+            },
+        },
+        messages: {
+            username: {
+                required: "Please enter a email address",
+                email: "Please enter a vaild email address"
+            },
+            password: {
+                required: "Please provide a password",
+                minlength: "Your password must be at least 5 characters long"
+            },
+            captcha: {
+                required: "Please enter captcha",
+                rangelength: "Captcha must be 4 characters long"
+            }
+        },
+        errorElement: 'span',
+        errorPlacement: function (error, element) {
+            error.addClass('invalid-feedback');
+            element.closest('.input-group').append(error);
+        },
+        highlight: function (element, errorClass, validClass) {
+            $(element).addClass('is-invalid');
+        },
+        unhighlight: function (element, errorClass, validClass) {
+            $(element).removeClass('is-invalid');
+        }
+    });
+
+    $("#imgCaptcha").on('click', loadCaptcha);
+    $("#imgCaptcha").trigger('click');
+})(jQuery)
---
+++ admin/js/resources.js
@@ -0,0 +1,232 @@
+const BRANCH_LIST = '/api/admin/branches';
+const RESOURCES_URI = '/api/admin/resources';
+const RESOURCE_TYPE_LIST = '/api/admin/resource_types';
+
+(function ($) {
+    'use strict'
+
+    var csrftoken = Cookies.get('csrfToken');
+    var dataTable;
+
+    function csrfSafeMethod(method) {
+        // these HTTP methods do not require CSRF protection
+        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    }
+    $.ajaxSetup({
+        beforeSend: function (xhr, settings) {
+            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader('x-csrf-token', csrftoken);
+            }
+        },
+    });
+
+    function loadInitData() {
+        dataTable = $('#tableResources').DataTable({
+            ajax: RESOURCES_URI,
+            responsive: true,
+            columns: [
+                { data: 'branch_id', visible: false},
+                { data: 'branch' },
+                { data: 'type' },
+                { data: 'tag' },
+                { data: 'service_ip' },
+                { data: 'service_domain' },
+                { data: 'service_url' },
+                { data: 'real_ip' },
+                { data: 'description' },
+                {
+                    data: 'enabled',
+                    render: function (data, type, row) {
+                        if (1 == data) {
+                            return '<i class="far fa-check-circle text-success"></i>';
+                        } else {
+                            return '<i class="fas fa-times-circle text-danger"></i>';
+                        }
+                    }
+                },
+                {
+                    data: 'id',
+                    render: function (data, type, row) {
+                        return `<a href="#" class="edit-button"><i class="far fa-edit"></i></a>
+                            &nbsp;&nbsp;&nbsp;&nbsp;
+                            <a href="#" class="del-button"><i class="far fa-trash-alt"></i></a>`;
+                    }
+                }
+            ]
+        });
+
+        dataTable.on('draw', () => {
+            $(".edit-button").on('click', (event) => {
+                edit(dataTable.row($(event.target).closest('tr')).data());
+            });
+
+            $(".del-button").on('click', (event) => {
+                delComfirm(dataTable.row($(event.target).closest('tr')).data());
+            });
+
+        });
+        $.getJSON(BRANCH_LIST)
+            .done(data => {
+                let selectBranchData = data.data.map(v => {
+                    return {
+                        id: v.id,
+                        text: v.name
+                    };
+                });
+                $("#selectBranch").select2({
+                    data: selectBranchData
+                });
+            }).fail((jqXHR, textStatus, err) => {
+                console.error(err);
+            });
+        $.getJSON(RESOURCE_TYPE_LIST)
+            .done(data => {
+                let selectResTypeData = data.data.map(v => {
+                    return {
+                        id: v.id,
+                        text: v.name
+                    };
+                });
+                $("#selectType").select2({
+                    data: selectResTypeData
+                });
+            }).fail((jqXHR, textStatus, err) => {
+                console.error(err);
+            });
+
+    }
+
+
+    $.validator.addMethod('IP4Checker', function (value) {
+        var ip = /^(\d|[1-9]\d|1\d\d|2([0-4]\d|5[0-5]))\.(\d|[1-9]\d|1\d\d|2([0-4]\d|5[0-5]))\.(\d|[1-9]\d|1\d\d|2([0-4]\d|5[0-5]))\.(\d|[1-9]\d|1\d\d|2([0-4]\d|5[0-5]))$/;
+
+        return '' === value || value.match(ip);
+    }, 'Invalid IP address');
+
+    $('#btnNewRes').on('click', () => {
+        $("#divError").hide();
+        $('#divEditor').modal('show');
+        $("input[name='id']").val(null);
+        $('#editorTitle').text('创建资源');
+        $('#formEditor')[0].reset();
+        $('.select2').trigger('change');
+    });
+
+    $('#btnDelete').on('click', (event) => {
+        $.ajax({
+            type: 'delete',
+            url: `${RESOURCES_URI}/${$(event.target).data('id')}`
+        }).done(result => {
+            if (result.result) {
+                $('#divDelete').modal('hide');
+                dataTable.ajax.reload();
+            } else {
+                $('#pDelError').text(result.msg);
+            }
+        }).fail((jqXHR, textStatus, err) => {
+            console.error(err);
+            $('#pDelError').text('删除数据失败');
+        });
+    });
+
+    function edit(data) {
+        $("#divError").hide();
+        $('#divEditor').modal('show');
+        $('#editorTitle').text('编辑资源');
+        $('#formEditor')[0].reset();
+        $('.select2').trigger('change');
+        $('#formEditor').jsonToForm(data);
+    }
+
+    function delComfirm(data) {
+        $('#divDelete').modal('show');
+        $('#spanDel').text(`${data.branch}-${data.type}-${data.tag?'(' + data.tag + ')':''}`);
+        $('#btnDelete').data('id', data.id);
+    }
+
+    function create(data) {
+        $.post(RESOURCES_URI, data).done(result => {
+            if (result.result) {
+                $('#divEditor').modal('hide');
+                dataTable.ajax.reload();
+            } else {
+                showErrMsg(result.msg);
+            }
+        }).fail((jqXHR, textStatus, err) => {
+            console.error(err);
+            showErrMsg('提交数据失败');
+        });
+    }
+
+    function update(data) {
+        $.ajax({
+            type: 'put',
+            url: `${RESOURCES_URI}/${data.id}`,
+            data: data
+        }).done(result => {
+            if (result.result) {
+                $('#divEditor').modal('hide');
+                dataTable.ajax.reload();
+            } else {
+                showErrMsg(result.msg);
+            }
+        }).fail((jqXHR, textStatus, err) => {
+            console.error(err);
+            showErrMsg('提交数据失败');
+        });
+    }
+
+    function showErrMsg(msg) {
+        $("#spanError").text(msg);
+        $("#divError").show();
+    }
+
+    $('#formEditor').validate({
+        submitHandler: function () {
+            let formData = new FormData($('#formEditor')[0]);
+            let data = Object.fromEntries(formData.entries());
+            data.enabled = ("on" === data.enabled ? 1 : 0);
+
+            if (typeof data.id == 'undefined' || !data.id) {
+                create(data);
+            } else {
+                update(data);
+            }
+        },
+        rules: {
+            service_ip: {
+                required: true,
+                IP4Checker: true
+            },
+            real_ip: {
+                required: false,
+                IP4Checker: true
+            }
+        },
+        messages: {
+            service_ip: {
+                required: "Please enter a service IP",
+                IP4Checker: "Please enter a vaild IP address"
+            },
+            real_ip: {
+                IP4Checker: "Please enter a vaild IP address"
+            }
+        },
+        errorElement: 'span',
+        errorPlacement: function (error, element) {
+            error.addClass('invalid-feedback');
+            element.closest('.form-group').append(error);
+        },
+        highlight: function (element, errorClass, validClass) {
+            $(element).addClass('is-invalid');
+        },
+        unhighlight: function (element, errorClass, validClass) {
+            $(element).removeClass('is-invalid');
+        }
+    });
+
+
+
+    loadInitData();
+
+})(jQuery)
